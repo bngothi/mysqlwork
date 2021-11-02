35a3f89148907506cf4a9f1cbcd18d52f15e3a97
bngothi <bngothi@aol.com>
2021-11-02 18:21:14 -0500

    Added a UI to view data in the database

---
+++ src/main/java/com/assignment_6/mysqlwork/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.assignment_6.mysqlwork;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<AddressBook, Long> {
+
+}
